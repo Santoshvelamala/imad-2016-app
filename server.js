dfc9fd5751348b60eb0c72027d632845ee89f579
Santoshvelamala <santhosh.v15@iiits.in>
2016-09-23 22:59:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/hii', function(req, res){
+    res.send("hii my dear");
+})
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
